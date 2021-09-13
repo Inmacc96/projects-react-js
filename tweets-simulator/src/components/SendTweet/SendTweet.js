911374c410969641e6c99d612e7373c6a3e6881e
inmacc96 <inmacaballerocarrero@gmail.com>
2021-09-13 14:50:11 +0200

    Adding the ModalContainer component to SendTweet and setting the IsOpenModel state to open and close the modal.

--- tweets-simulator/src/components/SendTweet/SendTweet.js
+++ tweets-simulator/src/components/SendTweet/SendTweet.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Fab } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import moment from "moment";
+import ModalContainer from "../ModalContainer";
 
 import "./SendTweet.scss";
 
 export default function SendTweet() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const openModal = () => {
+    setIsOpenModal(true);
+  };
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
   return (
     <div className="send-tweet">
-      <Fab className="send-tweet__open-modal" color="primary" aria-label="add">
+      <Fab
+        className="send-tweet__open-modal"
+        color="primary"
+        aria-label="add"
+        onClick={openModal}
+      >
         <AddIcon />
       </Fab>
+
+      <ModalContainer isOpenModal={isOpenModal} closeModal={closeModal}>
+        <p>Contenido del modal</p>
+      </ModalContainer>
     </div>
   );
 }
