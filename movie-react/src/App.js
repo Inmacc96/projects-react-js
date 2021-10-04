70803c9b1430f4a84a922d9e905619c97f171150
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-04 19:49:14 +0200

    Creating the routing system with React Router DOM.

--- movie-react/src/App.js
+++ movie-react/src/App.js
@@ -1,22 +1,50 @@
 import React from "react";
-import { Button } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
-import { Pagination } from "antd";
+import { Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+//Pages
+import Home from "./pages/home";
+import NewMovies from "./pages/new-movies";
+import Popular from "./pages/popular";
+import Search from "./pages/search";
+import Movie from "./pages/movie";
+import Error404 from "./pages/error404";
+
+export default function App() {
+  const { Header, Content } = Layout;
+
   return (
-    <div>
-      <h1>Estamos en App.</h1>
-      <Button type="primary" shape="circle" icon={<SearchOutlined />} />
-      <Button type="primary" shape="circle">
-        Inma
-      </Button>
-      <Button type="primary" icon={<SearchOutlined />}>
-        Search
-      </Button>
-      <Pagination defaultCurrent={1} total={50} />
-    </div>
+    <Layout>
+      <Router>
+        <Header>Header....</Header>
+
+        <Content>
+          {" "}
+          {/*  Contiene toda la informacion de la web */}
+          <Switch>
+            {/* Lo utilizamos para que decirle que cuando encuentre una página se detengan y cargue esa pagina y no continue buscando. */}
+            <Route path="/" exact={true}>
+              <Home />
+            </Route>
+            <Route path="/new-movies" exact={true}>
+              <NewMovies />
+            </Route>
+            <Route path="/popular" exact={true}>
+              <Popular />
+            </Route>
+            <Route path="/search" exact={true}>
+              <Search />
+            </Route>
+            <Route path="/movie/:id" exact={true}>
+              <Movie />
+            </Route>
+            <Route path="*" exact={true}>
+              {/* En cualquier ruta devuelve el componente error404, es decir, cuando no ha detectado las anteriores */}
+              <Error404 />
+            </Route>
+          </Switch>
+        </Content>
+      </Router>
+    </Layout>
   );
 }
-
-export default App;
