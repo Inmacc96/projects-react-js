16cdc4e3736a366c82caa713ebb9016fd2c688e9
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-04 19:09:29 +0200

    Testing some antd components.

--- movie-react/src/App.js
+++ movie-react/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Button } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { Pagination } from "antd";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Estamos en App.</h1>
+      <Button type="primary" shape="circle" icon={<SearchOutlined />} />
+      <Button type="primary" shape="circle">
+        Inma
+      </Button>
+      <Button type="primary" icon={<SearchOutlined />}>
+        Search
+      </Button>
+      <Pagination defaultCurrent={1} total={50} />
     </div>
   );
 }
