20b5e253a9d9eaca89fd84ddee97f5861df32ff9
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-07 19:53:39 +0200

    Adding a fragment of the film as background of the movie page.

--- movie-react/src/pages/movie/movie.js
+++ movie-react/src/pages/movie/movie.js
@@ -24,12 +24,22 @@ export default function Movie() {
 
 function RenderMovie(props) {
   const {
-    movieInfo: { title },
+    movieInfo: { title, backdrop_path },
   } = props;
 
+  const backdropPath = `https://image.tmdb.org/t/p/w500/${backdrop_path}`;
+
   return (
-    <div>
-      <h1>Estamos en la película {title} </h1>
+    <div className="movie" style={{ backgroundImage: `url(${backdropPath})` }}>
+      <div className="movie__dark" />
+      <Row>
+        <Col span={8} offset={3} className="movie__poster">
+          Caratula..
+        </Col>
+        <Col span={10} className="movie__info">
+          Movie Info...
+        </Col>
+      </Row>
     </div>
   );
 }
