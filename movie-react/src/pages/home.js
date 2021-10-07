f4153988aae2e503161004e8ec863a31b6672cf8
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-07 18:40:07 +0200

    Creating the MovieList component to display a list of movies.

--- movie-react/src/pages/home.js
+++ movie-react/src/pages/home.js
@@ -1,17 +1,28 @@
 import React from "react";
+import { Row, Col } from "antd";
 import useFetch from "../hooks/useFetch";
 import { URL_API, KEY_API } from "../utils/constans";
-
 import SliderMovies from "../components/SliderMovies";
+import MovieList from "../components/MovieList";
 
 export default function Home() {
   const newMovies = useFetch(
     `${URL_API}/movie/now_playing?api_key=${KEY_API}&language=es-ES&page=1`
   );
 
+  const popularMovies = useFetch(
+    `${URL_API}/movie/popular?api_key=${KEY_API}&language=es-ES&page=1`
+  );
+
   return (
     <>
       <SliderMovies movies={newMovies} />
+      <Row>
+        <Col span={12}>
+          <MovieList title="Peliculas populares" movies={popularMovies} />
+        </Col>
+        <Col span={12}>....</Col>
+      </Row>
     </>
   );
 }
