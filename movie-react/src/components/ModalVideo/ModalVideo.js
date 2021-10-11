cbde8778318aad7c0bdf47d3388da8efed49d789
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-11 19:35:36 +0200

    Loading trailer with ReactPlayer component.

--- movie-react/src/components/ModalVideo/ModalVideo.js
+++ movie-react/src/components/ModalVideo/ModalVideo.js
@@ -6,6 +6,22 @@ import "./ModalVideo.scss";
 
 export default function ModalVideo(props) {
   const { videoKey, videoPlatform, isOpen, close } = props; // isOpen es para saber si está abierto o no el modal y close una función para cerrar el modal.
+  const [urlVideo, setUrlVideo] = useState(null);
+
+  useEffect(() => {
+    switch (videoPlatform) {
+      case "YouTube":
+        setUrlVideo(`https://youtu.be/${videoKey}`);
+        break;
+      case "Vimeo":
+        setUrlVideo(`https://vimeo.com/${videoKey}`);
+        break;
+      default:
+        break;
+    }
+  }, [videoKey, videoPlatform]);
+
+  console.log(props);
 
   return (
     <Modal
@@ -15,7 +31,8 @@ export default function ModalVideo(props) {
       onCancel={close} // Para que el modal se cierre cuando pulsamos la x, se ejecuta la función close.
       footer={false}
     >
-      Este es mi modal
+      <ReactPlayer url={urlVideo} controls />
+      {/* playing={isOpen} */}
     </Modal>
   );
 }
