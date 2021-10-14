391fe308bcef484ac6ad15c96841ee21c9f9fbb9
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-14 17:26:04 +0200

    Testing the react-toastify dependencie.

--- cart-ecommerce/src/App.js
+++ cart-ecommerce/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { ToastContainer, toast } from "react-toastify";
+import "./App.css";
 
 function App() {
+  const openToast = () => {
+    toast("Este es mi toast!");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={openToast}>Click</button>
+      <ToastContainer />
     </div>
   );
 }
