c8f748074661eb85c49f404c003ec14379c05cb2
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-14 21:21:03 +0200

    Change of server to upload db.json

--- cart-ecommerce/src/utils/constants.js
+++ cart-ecommerce/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const urlApiProducts = "https://demo2562028.mockable.io/products";
+export const urlApiProducts = "https://api.jsonserve.com/zZ7r1f"; //https://jsonserve.com/
 export const BASE_PATH = "http://localhost:3000";
 export const STORAGE_PRODUCTS_CART = "productsCart";
