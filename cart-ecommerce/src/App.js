eb15b02045e5c639d2d2d8ee82ff4890a3b96d43
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-14 18:45:50 +0200

    Creating the hook useFecth to make http requests.

--- cart-ecommerce/src/App.js
+++ cart-ecommerce/src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TopMenu from "./components/TopMenu";
+import useFetch from "./hooks/useFetch";
+import { urlApiProducts } from "./utils/constans";
 
 function App() {
+  const result = useFetch(urlApiProducts);
+
+  console.log(result);
+
   return (
     <div>
       <TopMenu />
