59a46335f253d76be0d23119b627b056dd5fd14a
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-19 20:26:56 +0200

    Defining the function to open the shopping cart.

--- cart-ecommerce/src/components/Cart/Cart.js
+++ cart-ecommerce/src/components/Cart/Cart.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { ReactComponent as CartEmpty } from "../../assets/svg/cart-empty.svg";
 
 import "./Cart.scss";
 
 export default function Cart() {
+  const [cartOpen, setCartOpen] = useState(false);
+  const widthCartContet = cartOpen ? 400 : 0;
+
+  const openCart = () => {
+    setCartOpen(true);
+    document.body.style.overflow = "hidden"; //Cuando esté abierto el carrito no se puede hace scroll
+  };
+
+  const closeCart = () => {
+    setCartOpen(false);
+    document.body.style.overflow = "scroll";
+  };
+
   return (
     <>
       <Button variant="link" className="cart">
-        <CartEmpty />
+        <CartEmpty onClick={openCart} />
       </Button>
-      <div className="cart-content">Todos mis productos</div>
+      <div className="cart-content" style={{ width: widthCartContet }}>
+        Todos mis productos
+      </div>
     </>
   );
 }
