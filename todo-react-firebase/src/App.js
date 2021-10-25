323a89454e649ef40664103e84def6a8c929b8a8
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-25 20:46:52 +0200

    Creating the app header.

--- todo-react-firebase/src/App.js
+++ todo-react-firebase/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+import "./App.scss";
 
 export default function App() {
   return (
     <div>
-      <h1>Todo APP</h1>
+      <Container fluid className="app">
+        {/* fluid: para que la cabecera ocupe todo el ancho. */}
+        <div className="title">
+          <h1>Inma Caballero</h1>
+        </div>
+      </Container>
     </div>
   );
 }
