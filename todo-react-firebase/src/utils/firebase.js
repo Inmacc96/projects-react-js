ac53770dea3fc7416c17bf9f7d920bd6aa4f21fd
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-25 20:27:03 +0200

    Connecting Firebase to our react js application.

---
+++ todo-react-firebase/src/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbprY3lYYjjLKlf4y4XOE98_hFSGl1EEM",
+  authDomain: "todo-18474.firebaseapp.com",
+  projectId: "todo-18474",
+  storageBucket: "todo-18474.appspot.com",
+  messagingSenderId: "302709958397",
+  appId: "1:302709958397:web:969aaa86973802cc124094",
+};
+
+export default firebase.initializeApp(firebaseConfig);
