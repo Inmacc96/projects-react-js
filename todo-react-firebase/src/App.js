d2052a77c6f0aeca8bc9333aa322549a387abf1b
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-25 21:02:31 +0200

    Define and style the structure of the task list.

--- todo-react-firebase/src/App.js
+++ todo-react-firebase/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import "./App.scss";
 
@@ -9,8 +9,34 @@ export default function App() {
       <Container fluid className="app">
         {/* fluid: para que la cabecera ocupe todo el ancho. */}
         <div className="title">
-          <h1>Inma Caballero</h1>
+          <h1>Listado de tareas diarias</h1>
         </div>
+
+        <Row className="todo">
+          <Col
+            className="todo__title"
+            xs={{ span: 10, offset: 1 }}
+            md={{ span: 6, offset: 3 }}
+          >
+            {/*  Bootstrap tabaja con 12 columnas, lo anterior significa 
+         que va ocupar 6 columnas y tendra 3 columnas separadas de la derecha y otra 3 de la izquierda  */}
+            <h2>Today</h2>
+          </Col>
+          <Col
+            className="todo__list"
+            xs={{ span: 10, offset: 1 }}
+            md={{ span: 6, offset: 3 }}
+          >
+            <p> Lista de tareas</p>
+          </Col>
+          <Col
+            className="todo__input"
+            xs={{ span: 10, offset: 1 }}
+            md={{ span: 6, offset: 3 }}
+          >
+            <input placeholder="Nueva tarea" />
+          </Col>
+        </Row>
       </Container>
     </div>
   );
