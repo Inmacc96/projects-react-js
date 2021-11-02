cbbd8cd2e75aee699df5f13217576ebf96c31e15
inmacc96 <inmacaballerocarrero@gmail.com>
2021-11-02 19:34:36 +0100

    Saving the task in Firebase.

--- todo-react-firebase/src/components/AddTask/AddTask.js
+++ todo-react-firebase/src/components/AddTask/AddTask.js
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { isEmpty } from "lodash";
+import firebase from "../../utils/firebase";
+import "firebase/firestore";
 import { ReactComponent as Send } from "../../assets/send.svg";
 
 import "./AddTask.scss";
+
+//Inicializar la base de datos
+const db = firebase.firestore(firebase);
 
 export default function AddTask() {
   const [task, setTask] = useState("");
 
   const onSubmit = (e) => {
     e.preventDefault(); //Para que no se recargue la página
-    console.log("Formulario enviado...");
-    console.log(task);
+    if (!isEmpty(task)) {
+      // No podemos enviar task vacío a firebase. Por tanto lo validamos antes de enviarlo.
+      db.collection("tasks")
+        .add({
+          name: task,
+          completed: false,
+        }) // Es una promesa
+        .then(() => {
+          console.log("Tarea creada");
+        });
+    }
   };
+
   return (
     <Form onSubmit={onSubmit} className="add-task">
       <input
