d5119b70c35816e0fa539c39629c421a01f9cd7b
inmacc96 <inmacaballerocarrero@gmail.com>
2021-11-02 19:06:16 +0100

    Creating the task state to store the value of the form.

--- todo-react-firebase/src/components/AddTask/AddTask.js
+++ todo-react-firebase/src/components/AddTask/AddTask.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { ReactComponent as Send } from "../../assets/send.svg";
 
 import "./AddTask.scss";
 
 export default function AddTask() {
-  const onSubmit = () => {
+  const [task, setTask] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault(); //Para que no se recargue la página
     console.log("Formulario enviado...");
+    console.log(task);
   };
   return (
     <Form onSubmit={onSubmit} className="add-task">
-      <input type="text" placeholder="Nueva tarea..." />
+      <input
+        type="text"
+        placeholder="Nueva tarea..."
+        onChange={(e) => setTask(e.target.value)}
+      />
       <Button type="submit">
         <Send />
       </Button>
