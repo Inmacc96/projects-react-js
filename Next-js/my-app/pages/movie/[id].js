230ffbad1d66dfbc9fc9ab07615c7966203bd29b
inmacc96 <inmacaballerocarrero@gmail.com>
2021-11-09 13:07:44 +0100

    Adding a button to return to the home page.

--- Next-js/my-app/pages/movie/[id].js
+++ Next-js/my-app/pages/movie/[id].js
@@ -1,16 +1,22 @@
 import React from "react";
 import Menu from "../../components/Menu";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 
 export default function movie() {
   const router = useRouter();
   const { id } = router.query;
 
+  const goToHome = () => {
+    Router.push("/");
+  };
   return (
     <div>
       <Menu />
       <h2 style={{ textAlign: "center" }}>Estás viendo...</h2>
       <h1 style={{ textAlign: "center" }}>{id}</h1>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={goToHome}>Ir a inicio</button>
+      </div>
     </div>
   );
 }
