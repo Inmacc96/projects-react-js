08c08e9f48f43a64c5fd4d6c43195425ea9f3427
inmacc96 <inmacaballerocarrero@gmail.com>
2021-11-09 12:13:14 +0100

    Adding CSS in JS, External CSS and External SCSS.

---
+++ Next-js/my-app/components/CssExterno.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./CssExterno.module.css";
+
+export default function CssExterno() {
+  return <h1 className={styles.css_externo}> CSS Externo</h1>;
+}
---
+++ Next-js/my-app/components/ScssExterno.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./ScssExterno.module.scss";
+
+export default function ScssExterno() {
+  return <h1 className={styles.scss_externo}>SASS Externo</h1>;
+}
